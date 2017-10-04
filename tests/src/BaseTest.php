05a047379d24b98fd675ca0e1bfc703c6dd50292
Edwin Luijten <edwin.luijten@live.nl>
2017-10-04 16:35:10 +0200

    Added shop resource, added unit tests for client, shop and abstract resource.

---
+++ tests/src/BaseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ShopifyClient\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ShopifyClient\Client;
+use ShopifyClient\Config;
+
+abstract class BaseTest extends TestCase
+{
+    /**
+     * @var Client $client ;
+     */
+    public static $client;
+
+    public static function setUpBeforeClass()
+    {
+        self::$client = new Client(
+            new Config(
+                getenv('SHOPIFY_DOMAIN'),
+                getenv('SHOPIFY_KEY'),
+                getenv('SHOPIFY_SECRET')
+            )
+        );
+    }
+}
---
+++ tests/src/ClientTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace ShopifyClient\Tests;
+
+use ShopifyClient\Client;
+use ShopifyClient\Config;
+use ShopifyClient\Resource\Resource;
+
+class ClientTest extends BaseTest
+{
+    public function testCreateClient()
+    {
+        $client = new Client(
+            new Config(
+                getenv('SHOPIFY_DOMAIN'),
+                getenv('SHOPIFY_KEY'),
+                getenv('SHOPIFY_SECRET')
+            )
+        );
+
+        $this->assertInstanceOf(Client::class, $client);
+    }
+
+    /**
+     * @expectedException \ShopifyClient\Exception\ResourceException
+     */
+    public function testInvalidResourceConfigString()
+    {
+        new Client(
+            new Config(
+                getenv('SHOPIFY_DOMAIN'),
+                getenv('SHOPIFY_KEY'),
+                getenv('SHOPIFY_SECRET'),
+                [
+                    'foo' => 'bar',
+                ]
+            )
+        );
+    }
+
+    /**
+     * @expectedException \ShopifyClient\Exception\ResourceException
+     */
+    public function testInvalidResourceConfigArray()
+    {
+        new Client(
+            new Config(
+                getenv('SHOPIFY_DOMAIN'),
+                getenv('SHOPIFY_KEY'),
+                getenv('SHOPIFY_SECRET'),
+                [
+                    'foo' => [
+                        'bar' => 'baz',
+                    ],
+                ]
+            )
+        );
+    }
+
+    public function testGetResource()
+    {
+
+        $resource = static::$client->getResource('customers');
+
+        $this->assertInstanceOf(Resource::class, $resource);
+    }
+
+    /**
+     * @expectedException \ShopifyClient\Exception\ResourceException
+     */
+    public function testGetInvalidResource()
+    {
+        static::$client->getResource('foo');
+    }
+}
