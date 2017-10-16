0f04a2f7b14f992a64b95348011d2af96b8c9791
Edwin Luijten <edwin.luijten@live.nl>
2017-10-16 11:43:02 +0200

    PHP CS

--- src/Resource/DiscountCode.php
+++ src/Resource/DiscountCode.php
@@ -29,84 +29,14 @@ class DiscountCode extends AbstractResource implements Resource
     {
         parent::__construct($request);
 
-        $this->actions->add(
-            'create',
-            new Action(
-                Request::METHOD_POST,
-                'price_rules/%s/discount_codes.json',
-                'discount_code',
-                'discount_code'
-            )
-        );
-        $this->actions->add(
-            'createBatch',
-            new Action(
-                Request::METHOD_POST,
-                'price_rules/%s/batch.json',
-                'discount_code_creation',
-                'discount_codes'
-            )
-        );
-        $this->actions->add(
-            'get',
-            new Action(
-                Request::METHOD_GET,
-                'price_rules/%s/discount_codes/%s.json',
-                'discount_code',
-                'discount_code'
-            )
-        );
-        $this->actions->add(
-            'getBatch',
-            new Action(
-                Request::METHOD_GET,
-                'price_rules/%s/batch/%s.json',
-                'discount_code_creation',
-                'discount_code_creation'
-            )
-        );
-        $this->actions->add(
-            'all',
-            new Action(
-                Request::METHOD_GET,
-                'price_rules/%s/discount_codes.json',
-                'discount_codes',
-                'discount_codes'
-            )
-        );
-        $this->actions->add(
-            'allBatch',
-            new Action(
-                Request::METHOD_GET,
-                'price_rules/%s/batch/%s/discount_codes.json',
-                'discount_codes',
-                'discount_codes'
-            )
-        );
-        $this->actions->add(
-            'lookup',
-            new Action(
-                Request::METHOD_GET,
-                'discount_codes/lookup.json',
-                'discount_code',
-                'discount_code'
-            )
-        );
-        $this->actions->add(
-            'update',
-            new Action(
-                Request::METHOD_PUT,
-                'price_rules/%s/discount_codes/%s.json',
-                'discount_code',
-                'discount_code'
-            )
-        );
-        $this->actions->add(
-            'delete',
-            new Action(
-                Request::METHOD_DELETE,
-                'price_rules/%s/discount_codes/%s.json'
-            )
-        );
+        $this->actions->add('create', new Action(Request::METHOD_POST, 'price_rules/%s/discount_codes.json', 'discount_code', 'discount_code'));
+        $this->actions->add('createBatch', new Action(Request::METHOD_POST, 'price_rules/%s/batch.json', 'discount_code_creation', 'discount_codes'));
+        $this->actions->add('get', new Action(Request::METHOD_GET, 'price_rules/%s/discount_codes/%s.json', 'discount_code','discount_code'));
+        $this->actions->add('getBatch', new Action(Request::METHOD_GET, 'price_rules/%s/batch/%s.json', 'discount_code_creation', 'discount_code_creation'));
+        $this->actions->add('all', new Action(Request::METHOD_GET, 'price_rules/%s/discount_codes.json', 'discount_codes', 'discount_codes'));
+        $this->actions->add('allBatch', new Action(Request::METHOD_GET, 'price_rules/%s/batch/%s/discount_codes.json', 'discount_codes', 'discount_codes'));
+        $this->actions->add('lookup', new Action(Request::METHOD_GET, 'discount_codes/lookup.json', 'discount_code', 'discount_code'));
+        $this->actions->add('update', new Action(Request::METHOD_PUT, 'price_rules/%s/discount_codes/%s.json', 'discount_code', 'discount_code'));
+        $this->actions->add('delete', new Action(Request::METHOD_DELETE, 'price_rules/%s/discount_codes/%s.json'));
     }
 }
